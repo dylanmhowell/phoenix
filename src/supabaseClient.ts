e0179f8e70abeaa38ec23e983627a4e83ca41770
Dylan Howell <34950771+dylanmhowell@users.noreply.github.com>
2023-01-29 22:02:30 -0800

    chore(init supabase)

---
+++ src/supabaseClient.ts
@@ -0,0 +1,6 @@
+// src/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from './types'
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
---
+++ src/types.ts
@@ -0,0 +1,91 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      websites: {
+        Row: {
+          crawlTimestamp: string | null
+          h1: string | null
+          hash: string | null
+          id: number
+          indexibility: boolean | null
+          metaDescription: string | null
+          photobiz: boolean | null
+          pixieset: boolean | null
+          ranking: string | null
+          responseTime: number | null
+          searchTerm: string | null
+          showit: boolean | null
+          squarespace: boolean | null
+          statusCode: number | null
+          title: string | null
+          titleLength: number | null
+          url: string | null
+          webflow: boolean | null
+          wix: boolean | null
+          wordpress: boolean | null
+        }
+        Insert: {
+          crawlTimestamp?: string | null
+          h1?: string | null
+          hash?: string | null
+          id: number
+          indexibility?: boolean | null
+          metaDescription?: string | null
+          photobiz?: boolean | null
+          pixieset?: boolean | null
+          ranking?: string | null
+          responseTime?: number | null
+          searchTerm?: string | null
+          showit?: boolean | null
+          squarespace?: boolean | null
+          statusCode?: number | null
+          title?: string | null
+          titleLength?: number | null
+          url?: string | null
+          webflow?: boolean | null
+          wix?: boolean | null
+          wordpress?: boolean | null
+        }
+        Update: {
+          crawlTimestamp?: string | null
+          h1?: string | null
+          hash?: string | null
+          id?: number
+          indexibility?: boolean | null
+          metaDescription?: string | null
+          photobiz?: boolean | null
+          pixieset?: boolean | null
+          ranking?: string | null
+          responseTime?: number | null
+          searchTerm?: string | null
+          showit?: boolean | null
+          squarespace?: boolean | null
+          statusCode?: number | null
+          title?: string | null
+          titleLength?: number | null
+          url?: string | null
+          webflow?: boolean | null
+          wix?: boolean | null
+          wordpress?: boolean | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
