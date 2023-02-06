45159e1f4718984d471febdc5264672859bab7cc
Dylan Howell <34950771+dylanmhowell@users.noreply.github.com>
2023-02-06 11:56:10 -0800

    Chore: Add Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
-	},
+  integrations: [mdx(), tailwind()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  },
+  output: "server",
+  adapter: vercel()
 });
